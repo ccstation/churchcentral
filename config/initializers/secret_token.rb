e40b19159a9d0c9915e9b6d175600683a9ccf82f
ccstation <achan11@gmail.com>
2012-08-14 23:54:10 +0800

    new update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Churchcentral::Application.config.secret_token = 'eca0f3fc972a4696060196b961bd69a005ff08c95e0d6b8f0aa6aa455f8f1ea795346771e2c5b95597b026688ef5308ec6e230baad44ebeb5aad2c922109cac0'
